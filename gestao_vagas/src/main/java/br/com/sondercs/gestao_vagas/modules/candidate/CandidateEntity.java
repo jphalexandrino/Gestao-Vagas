f360bbbc2eebb91c609b7de65efa448837a62ead
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-29 21:16:14 +0000

    Adiciona a Criação de Tabela de Candidato com Spring Data JPA

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,28 +1,45 @@
 package br.com.sondercs.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data //Loombok depedency
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    // @Column(name = "nome") caso eu queira mudar o nome da coluna 
     private String name;
 
+    @NotBlank()
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço.")
     private String username;
 
-    @Email // Validação de Email
+    @Email(message = "O campo [email] deve conter um e-mail valido")
     private String email;
 
     @Length(min = 10, max = 100, message = "O Campo [password] deve conter entre 10 e 100 carácteres")
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAT;
+
 
 }
