8dfef605959dfc7d2f092474dbc4351e3969dc77
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-30 23:43:18 +0000

    Adicona o Controlador de Trabalhos (Job Controller))

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/entities/JobEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -24,13 +25,15 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
     
     @ManyToOne()// Uma Company para muitos Jobs
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id", nullable = false)
     private UUID companyId; //Mostra que tem relacionamento com outra entidade.
 
     @CreationTimestamp
---
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,18 @@
+package br.com.sondercs.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.sondercs.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.sondercs.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+    
+        @Autowired
+        private JobRepository jobRepository;
+
+        public JobEntity execute(JobEntity jobEntity){
+            return this.jobRepository.save(jobEntity);
+        }
+}
