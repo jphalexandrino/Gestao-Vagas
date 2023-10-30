f12dae9c38f0bacf6b7a3352aeb9d33db141d200
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-30 11:28:47 +0000

    Corrigido o problema da validação de erro.

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/controllers/CompanyController.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sondercs.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.sondercs.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/company")
@@ -17,7 +18,7 @@ public class CompanyController {
     private CreateCompanyUseCase companyUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity){
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity){
        try {
             var result = this.companyUseCase.execute(companyEntity);
             return ResponseEntity.ok().body(result);
