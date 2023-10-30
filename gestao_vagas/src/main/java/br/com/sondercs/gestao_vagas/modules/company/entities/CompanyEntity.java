c0c79d114f12a000dac52ab28ec208190b867590
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-30 01:33:03 +0000

    Criada a Entidade Company em Java.

---
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,44 @@
+package br.com.sondercs.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço.")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail valido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "O Campo [password] deve conter entre 10 e 100 carácteres")
+    private String password;
+
+    
+    private String website;
+    private String name;
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    
+}
