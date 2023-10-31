cb07a540e796e330799b215628ac2803c20c605d
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-31 21:45:48 +0000

    Adiciona

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,9 +1,13 @@
 package br.com.sondercs.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.sondercs.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.sondercs.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -12,6 +16,8 @@ import jakarta.security.auth.message.AuthException;
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -19,10 +25,10 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthException{
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthException{
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
-                throw new UsernameNotFoundException("Company not found");
+                throw new UsernameNotFoundException("Username/Password incorrect");
             });
             // Verificar a senha
             var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
@@ -31,5 +37,10 @@ public class AuthCompanyUseCase {
                 throw new AuthException();
             }
             // Se for igual
-    }
+            Algorithm algorithm = Algorithm.HMAC256("secretKey");
+            var token = JWT.create().withIssuer("javagas")
+            .withSubject(company.getId().toString())
+            .sign(algorithm);
+            return token;
+    }  
 }
