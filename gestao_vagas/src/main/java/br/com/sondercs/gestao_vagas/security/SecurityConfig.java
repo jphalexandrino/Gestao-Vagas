a167591f48e03cd87d6e6247b12a804b7e48a2d7
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-31 11:38:51 +0000

    Configurado Rotas Públicas com Spring Security em.

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/security/SecurityConfig.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/security/SecurityConfig.java
@@ -10,7 +10,13 @@ public class SecurityConfig {
 
     @Bean // Indica que um metodo dentro da classe esta sendo ultilizado para definir algum objeto já gerenciado
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth ->{
+            auth.requestMatchers("/candidate/").permitAll()
+            .requestMatchers("/company").permitAll();
+            auth.anyRequest().authenticated();
+        })
+        ;
         return http.build();
     }
 
