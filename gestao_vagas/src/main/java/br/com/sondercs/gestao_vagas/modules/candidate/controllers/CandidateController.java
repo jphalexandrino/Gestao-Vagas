ead7d816cd90bbfc96bb105300d120832669db0a
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-22 11:39:46 -0300

    Criando rota do perfil do candidato

--- gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package br.com.sondercs.gestao_vagas.modules.candidate.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sondercs.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.sondercs.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.sondercs.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -17,6 +22,9 @@ public class CandidateController {
 
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
+
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
     
     @PostMapping("/")
         public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
@@ -27,5 +35,18 @@ public class CandidateController {
                 return ResponseEntity.badRequest().body(e.getMessage());
             }
         }
-    }
-    
+
+
+        @GetMapping("/")
+        public ResponseEntity<Object> get(HttpServletRequest request) {
+          var idCandidate = request.getAttribute("candidate_id");
+      
+          try {
+            var profile = this.profileCandidateUseCase
+                .execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
+          } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+          }
+        }
+      }
