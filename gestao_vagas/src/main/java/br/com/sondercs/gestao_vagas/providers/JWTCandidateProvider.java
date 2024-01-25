8d251a979143a6b80a4862af758ecad240a62f50
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-24 22:22:55 -0300

    Criando `Filtro de segurança do candidato`

---
+++ gestao_vagas/src/main/java/br/com/sondercs/gestao_vagas/providers/JWTCandidateProvider.java
@@ -0,0 +1,33 @@
+package br.com.sondercs.gestao_vagas.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Service
+public class JWTCandidateProvider {
+
+    @Value("$P{${security.token.secret.candidate}}")
+    private String secretKey;
+
+    public DecodedJWT validateToken(String token){
+        token = token.replace("Bearer", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        try {
+            var tokenDecoded = JWT.require(algorithm)
+            .build()
+            .verify(token);
+            
+            return tokenDecoded;
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+}
