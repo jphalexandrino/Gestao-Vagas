aea538bba43ffc15bac3e8ac4b49a06cce735f2e
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-02-04 20:33:12 -0300

    Refaturando `Company Filter pt.1`

--- src/main/java/br/com/sondercs/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/sondercs/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.sondercs.gestao_vagas.modules.company.useCases;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 import javax.naming.AuthenticationException;
 
@@ -14,6 +15,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.sondercs.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.sondercs.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import br.com.sondercs.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
@@ -28,7 +30,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
                 throw new UsernameNotFoundException("Username/Password incorrect");
@@ -42,11 +44,19 @@ public class AuthCompanyUseCase {
             // Se for igual
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
+            var expires_in = Instant.now().plus(java.time.Duration.ofHours(2));
+
             var token = JWT.create().withIssuer("javagas")
-            .withExpiresAt(Instant.now().plus(java.time.Duration.ofHours(2)))
             .withSubject(company.getId().toString())
+            .withExpiresAt(expires_in)
+            .withClaim("roles", Arrays.asList("COMPANY"))
             .sign(algorithm);
 
-        return token;
+            var AuthCandidateResponseDTO = AuthCompanyResponseDTO.builder()
+            .access_token(token)
+            .expires_in(expires_in.toEpochMilli())
+            .build();
+
+        return AuthCandidateResponseDTO;
     }  
 }
